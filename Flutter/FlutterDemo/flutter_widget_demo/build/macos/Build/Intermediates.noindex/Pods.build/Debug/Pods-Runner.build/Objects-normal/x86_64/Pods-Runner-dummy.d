d9fc92f586ba7f82597fe4392d1e3b22638443d3
oakvillechen <guodong.chen@outlook.com>
2023-01-20 15:18:33 -0500

    add new folders
    add new folders

---
+++ Flutter/FlutterDemo/flutter_widget_demo/build/macos/Build/Intermediates.noindex/Pods.build/Debug/Pods-Runner.build/Objects-normal/x86_64/Pods-Runner-dummy.d
@@ -0,0 +1,3 @@
+dependencies: \
+  /Users/guodong.chen/Desktop/MyGitHub/FlutterDemo/flutter_widget_demo/macos/Pods/Target\ Support\ Files/Pods-Runner/Pods-Runner-dummy.m \
+  /Users/guodong.chen/Desktop/MyGitHub/FlutterDemo/flutter_widget_demo/build/macos/Build/Intermediates.noindex/Pods.build/Debug/Pods-Runner.build/module.modulemap
---
+++ Flutter/FlutterDemo/flutter_widget_demo/build/macos/Build/Intermediates.noindex/Pods.build/Debug/Pods-Runner.build/Objects-normal/x86_64/Pods_Runner_vers.d
@@ -0,0 +1,2 @@
+dependencies: \
+  /Users/guodong.chen/Desktop/MyGitHub/FlutterDemo/flutter_widget_demo/build/macos/Build/Intermediates.noindex/Pods.build/Debug/Pods-Runner.build/DerivedSources/Pods_Runner_vers.c
